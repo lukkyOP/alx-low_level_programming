4e15731a1996c1db9839cbe44493e2b541d2080d
lukkyOP <lukmon137@gmail.com>
2022-03-12 22:22:56 +0100

    My commit message for task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
-putchar(lower_case)
+putchar(lower_case);
 }
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
