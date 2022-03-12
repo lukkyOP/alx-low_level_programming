377dcefed95016ddc5f1692b777f939a26f17e73
lukkyOP <lukmon137@gmail.com>
2022-03-12 23:53:51 +0100

    My commit message for task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/
+[A[C**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int tens;
+int ones;
+
+
+for (tens = o; tens <= 9; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(tens + '0');
+putchar(ones + '0')
+[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D;
+
+
+if (tens < 8)
+{
+putchar(',');
+putchar(',');
+}
+}
+}
+putchar('\n');
+
+
+return[A[A[B[B (0);
+
+}
