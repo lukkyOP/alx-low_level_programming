3fc2fd1fd69dbe0b1650f63337c1b7602603c469
lukkyOP <lukmon137@gmail.com>
2022-03-12 15:24:00 +0100

    My commit message for task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
-#include <studio.h>
-
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (success/correct)
  */
+
 int main(void)
 {
-        int n;
+int n;
 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-[A[A[A[B[A[B[B        strand(time(0));
-        n = rand() - RAND_MAX / 2;
+if (n == 0)
+{
+printf("%i is zero\n", n);
+}
+else if (n<0)
+{printf("%i is negative\n", n);
+}
 
-        if (n == 0)
-                printf( "%d is zero\n", n);
-        else if (n < 0)
-                printf("%d[D[D[D [C"%d is negative\n", n);
-        else
-                print( "%d is positive\n", n);
+else
+{
+printf("%i is positive\n", n);
+}
 
-        return (0);[A[D[D[D[D[D[D[D[D[D[D[A[A[C[C[C[Cif (n > 0)[B[B[B[B
+return (0);
+
+}
