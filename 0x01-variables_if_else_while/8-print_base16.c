1398f3bdff4cf4d0064c8e78d7d09db40652bda5
lukkyOP <lukmon137@gmail.com>
2022-03-12 23:25:44 +0100

    My commit message for task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,17 @@
  * Return: Always 0 (success/correct)
  */
 
-[Aint main(void)
+int main(void)
 
 {
 char base_16;
 
-for (base_16 = 48; base_16 <= 102; base_16++)
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+
+for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
 putchar(base_16);
 }
