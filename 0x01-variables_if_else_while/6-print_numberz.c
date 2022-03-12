921c93ae17f0165447c1a98e1ced039e5544a9a7
lukkyOP <lukmon137@gmail.com>
2022-03-12 22:58:08 +0100

    My commit message for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ [A[C*[A[B* main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
