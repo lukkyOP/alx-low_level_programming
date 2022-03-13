3ca6c8a2dcdddadfe8d55fc5c6c78e5e3fd0bef7
lukkyOP <lukmon137@gmail.com>
2022-03-13 13:10:42 +0100

    My commit message for task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Return 1 after compiling
+ */
+int main(void)
+{
+write("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
+return (0);
+}
