7adbdf7cbf8f8a64b7bbc42b313cebf26090417e
lukkyOP <lukmon137@gmail.com>
2022-03-13 12:05:20 +0100

    My commit message for task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
-
-[A *
- * Return: Always 0 (success/correct)
+ *main - Entry point
+ *
+ *Return: return 1 after program execution
  */
-
-int main(void)[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Creturn 1 after program execution[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
