df9eb6cd3db021ae2232e29547baa1d7b1f957ad
lukkyOP <lukmon137@gmail.com>
2022-03-17 00:43:27 +0100

    My commit message for task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet - print alphabet in lowercase.
  *
-* Return: void.
+ * Return: void.
  */
 
 void print_alphabet(void)
@@ -15,6 +15,6 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 _putchar(alphabet);
 }
 
-_putchar('\n);
+_putchar('\n');
 
 }
