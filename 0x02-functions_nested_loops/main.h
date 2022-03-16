40dc08c9125da32cad6222f1b12d1e48bf77f987
lukkyOP <lukmon137@gmail.com>
2022-03-16 13:26:16 +0100

    My commit message for task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
-void print_alphabet_x1(void);[B[B[A[C[C[C[C
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
