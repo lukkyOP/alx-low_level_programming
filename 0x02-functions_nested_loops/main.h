3dba980f8d5e0c0eb1d36d57fbc2ce34f701a285
lukkyOP <lukmon137@gmail.com>
2022-03-16 13:14:14 +0100

    My commit message for Main file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet_x10(void);
+void print_alphabet_x1(void);[B[B[A[C[C[C[C
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
