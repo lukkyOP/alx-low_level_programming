ee486eb615f3ce7a34f3c608f058b4735d92a77a
lukkyOP <lukmon137@gmail.com>
2022-03-16 13:21:44 +0100

    My commit message for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return
+ */[A[C[C[C[C[C[C: nothing, return void[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
