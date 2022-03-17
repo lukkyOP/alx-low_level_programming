0e4836872d0e2d230e180f32450ea850a1fc76c4
lukkyOP <lukmon137@gmail.com>
2022-03-17 22:25:02 +0100

    My commit message for task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * mul(int a, int b) - multiplies two integers
+ *@c: print int
+[A[C[C Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+int a = i
+int b = [A'i'[B[D[D[D'j'
+{
+printf( 'i' * 'j' )
+}[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+
+
+retu[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[ "%d" 'i' * 'j' );
+
+
+[C[C[C[Crn (0);
+
+}
