8aea6a0594dc5a62d71484302b75fe8cb65fc6eb
lukkyOP <lukmon137@gmail.com>
2022-03-17 21:47:30 +0100

    My commit message for task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print _number(void);
+void print _most_numbers(void);
+void more _numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);[A[A[A[_numbers(void);[A[C[C[C[C_most_numbers(void);[A_numbers(void);[B[B[B[B[B[B[C[C[C[C[C
