10bb1c1bd3b0d0ad1f07378f3db69fd5c3d71f90
lukkyOP <lukmon137@gmail.com>
2022-09-09 21:50:19 +0000

    my commit for task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,6 +5,8 @@
  * Return: Always 0 (success/correct)
  */
 int main(void)
+
+
 {
 int tens;
 int ones;
@@ -14,20 +16,20 @@ for (tens = 0; tens <= 9; tens++)
 {
 for (ones = tens + 1; ones <= 9; ones++)
 {
-putchar(tens + '0');
-putchar(ones + '0');
+putchar (tens + '0');
+putchar (ones + '0');
 
 
 if (tens < 8)
 {
 putchar(',');
-putchar(' ');
+putchar (' ');
 }
 }
 }
-putchar('\n');
+putchar ('\n');
 
 
-return(0);
+return (0);
 
 }
