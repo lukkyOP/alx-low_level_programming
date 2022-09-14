bf0f8dabbc9a6cab0fa8c56f3eed1d714afc4580
lukkyOP <lukmon137@gmail.com>
2022-09-14 10:27:46 +0000

    0x03-debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 int i;
 
 
-printf("Infinite loop incoming :(\n");
+/*printf("Infinite loop incoming :(\n");*/
 
 
 i = 0;
@@ -21,9 +21,9 @@ i = 0;
  *{
  *putchar(i);
  *}
- *
- *printf("Infinite loop avoided! \\o/\n");
  */
+  printf("Infinite loop avoided! \\o/\n");
+ 
 
 return (0);
 }
