fad052a4af7f9e7a782fb926920285f0a4fa92ac
lukkyOP <lukmon137@gmail.com>
2022-09-16 17:09:30 +0000

    print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main -a function that prints a square
- *
+ * main - function that prints a square
+ * @size: sixe of square
  * Return: Always 0.
  */
 
@@ -12,7 +12,7 @@ void print_square(int size);
 	int i;
 	int k;
 
-	if (size > 0)
+	if (size <= 0)
 	{
 		for (i = 0; i < size; i++)
 		{
