d209dbe6611aead0ac3f42f86a478a52ebfa1226
lukkyOP <lukmon137@gmail.com>
2022-09-20 00:11:44 +0000

    pointers, strings and arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: A pointer to an int that will be changed
+ * @n: return value of n
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(",");
+		}
+
+		i++;
+	}
+
+	printf("\n");
+}
+
