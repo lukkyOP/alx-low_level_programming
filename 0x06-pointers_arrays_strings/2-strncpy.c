da9d4c9183237cb1ca50359bc8f9811512b21889
lukkyOP <lukmon137@gmail.com>
2022-09-21 23:11:31 +0000

    pointers, strings and arrays

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,12 +4,13 @@
  * _strncpy - copies a string
  * @dest: A pointer to a character that will be changed
  * @src: A pointer to a character that will be changed
+ * @n: value
  * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, n;
+	int i;
 
 	i = 0;
 	while (i < n && src[i] != '\0')
