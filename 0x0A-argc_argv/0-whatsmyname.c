20e81d56218d44befce8e3ba9fc9fc693174e8f8
lukkyOP <lukmon137@gmail.com>
2022-10-03 22:05:41 +0000

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
  * Return: success
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
-		printf("%s\n" , argv[0]);
-	
-return(0);
+		printf("%s\n", argv[0]);
+return (0);
 }
