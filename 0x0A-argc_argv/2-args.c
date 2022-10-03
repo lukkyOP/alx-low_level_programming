6684705f0c6a394f04abe58fc62dca8105f72364
lukkyOP <lukmon137@gmail.com>
2022-10-03 22:30:32 +0000

    argc argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,7 +11,15 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv);
+	int i = 0;
+
+	if (argc > 0)
+	{
+		while (i < argc)
+		{
+	printf("%s\n", argv[i]);
+	i++;
+		}
+	}
 	return(0);
 }
