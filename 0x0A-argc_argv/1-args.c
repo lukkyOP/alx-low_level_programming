f32796e081c11d27d35acdc1ef54a4bc254d96e3
lukkyOP <lukmon137@gmail.com>
2022-10-03 22:21:34 +0000

    argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,14 +4,14 @@
 /**
  * main - prints the number of arguments passed into it
  * @argc: argument counts
- * @argv arguments
+ * @argv: arguments
  *
- * Return: 0 
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
+	(void) argv; /* void argv */
 	printf("%i\n", argc - 1);
 	return (0);
 }
