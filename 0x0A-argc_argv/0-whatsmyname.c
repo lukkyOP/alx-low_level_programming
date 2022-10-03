d115fdcddf137eb0b5955b7fc8d53be34c37f901
lukkyOP <lukmon137@gmail.com>
2022-10-03 21:57:36 +0000

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlo.h>
+#include "main.h"
+
+/**
+ * main - Entry point A program that prints its name
+ * @argc: argument count
+ * @argv: arguments
+ * Return: success
+ */
+
+int main(int argc, char * argv[])
+{
+	(void) argc;
+		printf("%s\n" , argv[0]);
+	
+return(0);
+}
