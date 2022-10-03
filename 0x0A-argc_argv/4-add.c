e676ba2353ee781b3571b597cbb96641cd875038
lukkyOP <lukmon137@gmail.com>
2022-10-03 23:17:32 +0000

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,18 +14,15 @@
 int check_num(char *str)
 
 {
-	unsigned int count;
+	int count;
 
-	count = 0;
-	while (count < strlen(str))
-
+	for (count = 0; str[count] != '\0'; count++)
+	
 	{
-		if (!isdigit(str[count]))
+		if (str[count] < '0' || str[count] > '9')
 		{
 			return (0);
 		}
-
-		count ++;
 	}
 	return (1);
 }
@@ -38,31 +35,30 @@ int check_num(char *str)
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 
 {
-	int count;
-	int str_to_int;
-	int sum = 0;
+	int count, sum;
+	
+	sum = 0;
 
-	count = 1;
-	while (count < argc)
+	for (count = 1; count < argc; count++)
+
 	{
-		if(check_num(argv[count]))
+		if (check_num(argv[count]) == 0)
 
 		{
-			str_to_int = atoi(agrv[count]);
-			sum += str_to_int;
+			printf("Error\n");
+			return (1);
 		}
-		else
+		if (atoi(argv[count]) <= 0)
 		{
 			printf("Error\n");
 			return (1);
 		}
 
-		count++;
+		sum += atoi(argv[count]);
 	}
-
 	printf("%d\n", sum);
 
 	return (0);
