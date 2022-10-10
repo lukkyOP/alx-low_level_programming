600132c76593a96c4e8a0013de2d8bbca87a65f1
lukkyOP <lukmon137@gmail.com>
2022-10-10 13:26:27 +0000

    structs and typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,13 +10,6 @@
  * Description: This struct is for dog
  */
 
-struct dog {
-*d;
-char *name;
-float age;
-char *owner;
-}
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
