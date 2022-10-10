8f9207ad4d2870c35d9bdeb63a51dd30b5d180ab
lukkyOP <lukmon137@gmail.com>
2022-10-10 13:19:19 +0000

    structs and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: name
  * @age: age
  * @owner: owner
- *
  * Description: This struct is for dog
  */
 
@@ -14,16 +13,6 @@ struct dog {
 	char *name;
 	float age;
 	char *owner;
-}
-{
-int main(void)
-	struct dog dog_t;
-
-	my_dog.name = "lucky";
-	my_dog.age = 3;
-	my_dog.owner = "Tom";
-
-	return (0);
 };
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
